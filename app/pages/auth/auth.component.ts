67d2f40e9c589abf743952f360aa06ef755d52b6
Durvesh Patil <durvesh012345@gmail.com>
2024-07-07 16:46:20 +0530

    auth register/signup page

--- app/pages/auth/auth.component.ts
+++ app/pages/auth/auth.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
+  isRegister=false;
 
+  recipeItem:any={
+    title:"",
+    description:"",
+    foodType:"",
+    image:"",
+  }
+  onSubmit(){
+    console.log("values",this.recipeItem)
+  }
 }
